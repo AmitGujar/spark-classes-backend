1b0c1656def180ef387c2feec05c238a11f619a9
Prathamesh More <moreprathameshmore@hotmail.com>
2020-01-03 14:50:41 +0530

    Add ejs views

---
+++ src/routes/homepage.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (request, response, next) => {
+    response.status(200).render('homepage');
+});
+
+router.get('/signin', (request, response, next) => {
+    response.status(200).render('signin');
+});
+
+router.get('/contact', (request, response, next) => {
+    response.status(200).render('contact');
+});
+
+module.exports = router;
